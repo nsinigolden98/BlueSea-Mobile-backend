d915ebf42b7a7d14bcc10a3b9bde8c53521902fb
Right Fix <righteousnessude@gmail.com>
2025-09-19 21:13:40 +0100

    update bluesea_mobile/urls.py

--- bluesea_mobile/urls.py
+++ bluesea_mobile/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('group_payment/', include('group_payment.urls')),
 ]
