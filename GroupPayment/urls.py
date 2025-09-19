573293c019f77e277e7c9dd16c516171b88314ed
Right Fix <righteousnessude@gmail.com>
2025-09-19 21:30:00 +0100

    update GroupPayment/urls.py

--- GroupPayment/urls.py
+++ GroupPayment/urls.py
@@ -5,8 +5,6 @@ app_name = 'GroupPayment'
 
 
 urlpatterns = [
-    path('register/', views.RegisterView.as_view(), name='register'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('verify-email/', views.VerifyEmail.as_view(), name='verify-email'),
-    path('resend-otp/', views.ResendOtp.as_view(), name='resend-otp'),
+    #path('/', , name=''),
+    
 ]
