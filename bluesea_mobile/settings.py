9285a515267c7ebe02b058895e08f7cf533c27f5
RightFix <righteousnessude@gmail.com>
2025-09-20 10:15:10 +0100

    added the transctiom app to the setting.py

--- bluesea_mobile/settings.py
+++ bluesea_mobile/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'accounts',
     'wallet',
     'group_payment',
+    'transactions',
 ]
 
 MIDDLEWARE = [
