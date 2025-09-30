ac3252f9a4daad8f10f8e61aa6038513cfe3f0c4
Covenant Monday <covenantmonday863@gmail.com>
2025-09-30 22:38:54 +0100

    fixed debit error

--- payments/views.py
+++ payments/views.py
@@ -15,6 +15,7 @@ from wallet.models import Wallet
 from .vtpass import *
 
 class AirtimeTopUpViews(APIView):
+    permission_classes = [IsAuthenticated]
     
     def post(self, request):
         serializer = AirtimeTopUpSerializer(data = request.data)
@@ -32,69 +33,92 @@ class AirtimeTopUpViews(APIView):
                         "amount": amount,
                         "phone": serializer.data["phone_number"]
                     }
+                    user_wallet = request.user.wallet
                     # Wallet.debit(amount, ref_id)
                     buy_airtime_response = top_up(data)
                     if buy_airtime_response.get("response_description") == "TRANSACTION SUCCESSFUL":
-                        Wallet.debit(amount, reference=request_id)
+                        user_wallet.debit(amount=amount, reference=request_id)
                     
                     return Response(buy_airtime_response)
-                    
+
+
 class ElectricityPaymentViews(APIView):
+    permission_classes = [IsAuthenticated]
     def post(self, request):
         serializer = ElectricityPaymentSerializer(data = request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             with transaction.atomic():
-                amount = serializer.data["amount"]
+                request_id = generate_reference_id()
+                amount = int(serializer.data["amount"])
                 data = {
-                        "request_id": serializer.data["reference_id"],
+                        "request_id": request_id,
                         "serviceID": serializer.data["biller_name"],
                         "billerCode": serializer.data["billerCode"],
                         "variation_code": serializer.data["meter_type"],
                         "amount": amount,
                         "phone": serializer.data["phone_number"]
                     }
-                Wallet.debit(amount) 
+                # Wallet.debit(amount) 
+                user_wallet = request.user.wallet
+
                 electricity_response = top_up(data)
+                if electricity_response.get("response_description") == "TRANSACTION SUCCESSFUL":
+                    user_wallet.debit(amount=amount, reference=serializer.data["reference_id"])
                 return Response(electricity_response)
                 
+
 class WAECRegitrationViews(APIView):
+    permission_classes = [IsAuthenticated]
     def post(self, request):
         serializer = WAECRegitrationSerializer(data = request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             with transaction.atomic():
+                request_id = generate_reference_id()
                 amount = 14500
                 data = {
-                        "request_id": serializer.data["reference_id"],
+                        "request_id": request_id,
                         "serviceID": "waec-registration",
                         "variation_code": "waec-registration",
                         "quantity" : 1,
                         "phone": serializer.data["phone_number"]
                     }
-                Wallet.debit(amount) 
+                # Wallet.debit(amount)
+                user_wallet = request.user.wallet 
                 registration_response = top_up(data)
+                if registration_response.get("response_description") == "TRANSACTION SUCCESSFUL":
+                    user_wallet.debit(amount=amount, reference=request_id)
                 return Response(registration_response)
                 
+
 class WAECResultCheckerViews(APIView):
+    permission_classes = [IsAuthenticated]
     def post(self, request):
         serializer = WAECResultCheckerSerializer(data = request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             with transaction.atomic():
+                request_id = generate_reference_id()
                 amount = 950
                 data = {
-                        "request_id": serializer.data["reference_id"],
+                        "request_id": request_id,
                         "serviceID": "waec",
                         "variation_code": "waecdirect",
                         "quantity" : 1,
                         "phone": serializer.data["phone_number"]
                     }
-                Wallet.debit(amount) 
+                # Wallet.debit(amount)
+                user_wallet = request.user.wallet
+
                 registration_response = top_up(data)
+                if registration_response.get("response_description") == "TRANSACTION SUCCESSFUL":
+                    user_wallet.debit(amount=amount, reference=request_id)
                 return Response(registration_response)
                 
-class JAMBRegistrationViews(APIView):                   
+
+class JAMBRegistrationViews(APIView):
+    permission_classes = [IsAuthenticated]
     def post(self, request):
         serializer = JAMBRegistrationSerializer(data = request.data)
         
@@ -102,15 +126,19 @@ class JAMBRegistrationViews(APIView):
             serializer.save()
             
             with transaction.atomic():
+                request_id = generate_reference_id()
                 amount = 7700 if serializer.data["exam_type"] == 'utme-mock' else 6200
                 data = {
-                        "request_id": serializer.data["reference_id"],
+                        "request_id": request_id,
                         "serviceID": "jamb",
                         "variation_code": serializer.data["exam_type"],
                         "billerCode" : serializer.data["billerCode"],
                         "phone": serializer.data["phone_number"]
                     }
-                Wallet.debit(amount) 
+                # Wallet.debit(amount)
+                user_wallet = request.user.wallet 
                 jamb_registration_response = top_up(data)
+                if jamb_registration_response.get("response_description") == "TRANSACTION SUCCESSFUL":
+                    user_wallet.debit(amount=amount, reference=request_id)
                 return Response(jamb_registration_response)
                 
