ef0deaa87827653b8fac6c6fc2943a01e47ffe76
Covenant Monday <covenantmonday863@gmail.com>
2025-10-08 14:07:01 +0100

    wrapped up croup payments

--- payments/urls.py
+++ payments/urls.py
@@ -13,7 +13,7 @@ from .views import (
     AirtelDataTopUpViews,
     GloDataTopUpViews,
     EtisalatDataTopUpViews,
-    # GroupPaymentViews,
+    GroupPaymentViews,
     )
 
 urlpatterns = [
@@ -30,6 +30,6 @@ urlpatterns = [
     path('jamb-registration/', JAMBRegistrationViews.as_view(), name='jamb-registration'),
     path('waec-result/', WAECResultCheckerViews.as_view(), name='waec-result-checker'),
     path('waec-registration/', JAMBRegistrationViews.as_view(), name='waec-registration'),
-    # path('group-payment/', GroupPaymentViews.as_view(), name='group-payment'),
+    path('group-payment/', GroupPaymentViews.as_view(), name='group-payment'),
     #path('airtime-buyback/', name='airtime-buyback'),
 ]
