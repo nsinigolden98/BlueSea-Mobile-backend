da3190665370ce5da2c8870063c520083d5e249e
RightFix <righteousnessude@gmail.com>
2025-11-01 16:19:54 +0100

    test

--- user_preference/serializers.py
+++ user_preference/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import UserPreference, THEME_CHOICES
 
 class UserPreferenceSerializer(serializers.ModelSerializer):
+#     email = serializers.SerializerMethodField(read_only=True)
+#     def email(self, obj):
+#         return obj.user.email
     theme_color = serializers.ChoiceField(
         choices=THEME_CHOICES
     )
@@ -9,3 +12,4 @@ class UserPreferenceSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserPreference
         fields = ['theme_color']
+        #read_only_fields = True
--- user_preference/views.py
+++ user_preference/views.py
@@ -1,16 +1,23 @@
 from rest_framework import generics, permissions
+from rest_framework.exceptions import NotFound
 from .models import UserPreference
 from .serializers import UserPreferenceSerializer
 
-class UserPreferenceView(generics.RetrieveUpdateAPIView):
+class UserPreferenceView(generics.RetrieveUpdateDestroyAPIView):
 
     serializer_class = UserPreferenceSerializer
-    # Requires the user to be logged in to access
+    # Only authenticated users can access their preferences
     permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
-
         user = self.request.user
-        preference, created = UserPreference.objects.get_or_create(user=user)
-        
+        if not user.is_authenticated or not user.is_active:
+             raise NotFound("User is not authenticated.")
+             
+        try:
+            preference = UserPreference.objects.get(user=user)
+        except UserPreference.DoesNotExist:
+       
+            preference = UserPreference.objects.create(user=user)
+            
         return preference
